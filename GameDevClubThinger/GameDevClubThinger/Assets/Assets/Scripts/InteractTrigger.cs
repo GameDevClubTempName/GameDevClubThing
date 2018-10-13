2dbcfacd5ae09f063c7152bdfe957c1b609e4e37
ShinyLogarius <ShinyLogarius@users.noreply.github.com>
2018-10-12 22:02:15 -0400

    Basic Interactions Setup
    A basic framework for the player character activating effects on interactable objects

---
+++ GameDevClubThinger/GameDevClubThinger/Assets/Assets/Scripts/InteractTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractTrigger : MonoBehaviour {
+
+	private PlayerController playerController;
+	private GameObject parentGameObject;
+
+	void Awake() {
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		if (player == null) {
+			Debug.Log ("Player not found");
+		} else {
+			playerController = player.GetComponent<PlayerController> ();
+		}
+
+		parentGameObject = transform.parent.gameObject;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Player")) {
+			Debug.Log ("Player entered");
+			playerController.EnterInteraction (parentGameObject);
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.CompareTag ("Player")) {
+			Debug.Log ("Player exited");
+			playerController.ExitInteraction (parentGameObject);
+		}
+	}
+}
--- GameDevClubThinger/GameDevClubThinger/Assets/Assets/Scripts/PlayerController.cs
+++ GameDevClubThinger/GameDevClubThinger/Assets/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 0.5f;
 
 	private Transform selfTransform;
+	private int interactNear = 0;
+	private bool canInteract = false;
+	public List<Interactable> interactables = new List<Interactable>();
+	public List<Transform> interTransform = new List<Transform>();
 
 	void Start () {
 		selfTransform = this.GetComponent<Transform> ();
@@ -25,5 +29,50 @@ public class PlayerController : MonoBehaviour {
 
             selfTransform.position = new Vector3(tempX, tempY, tempZ);
         }
+
+		if (Input.GetKeyDown ("left shift")) {
+			if (canInteract) {
+				CallInteraction ();
+			}
+		}
     }
+
+	private void CallInteraction() {
+		float closestDistance;
+		Vector3 selfPosition = transform.position;
+		int interCount = interactables.Count;
+		int index = 0;
+
+		closestDistance = Vector3.Distance(selfPosition, interTransform[0].position);
+		if (interCount != 0) {
+			for (int i = 1; i < interCount; i++) {
+				float tempDistance = Vector3.Distance(selfPosition, interTransform[0].position);
+				if (tempDistance < closestDistance) {
+					closestDistance = tempDistance;
+					index = i;
+				}
+			}
+		}
+		interactables [index].Interaction ();
+	}
+
+	public void EnterInteraction(GameObject inter) {
+		interactNear++;
+		canInteract = true;
+		interactables.Add (inter.GetComponent<Interactable>());
+		interTransform.Add (inter.GetComponent<Transform> ());
+	}
+
+	public void ExitInteraction(GameObject inter) {
+		interactNear--;
+		if (interactNear == 0) {
+			canInteract = false;
+		} else if (interactNear < 0) {
+			Debug.Log ("interactNear == " + interactNear + " ????");
+		}
+		interactables.Remove (inter.GetComponent<Interactable>());
+		interTransform.Remove (inter.GetComponent<Transform> ());
+	}
+
+
 }
